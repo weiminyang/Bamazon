e3e170962c10b3592cacfe16e77038156388733a
weimin yang <yangwm1983@gmail.com>
2019-07-07 01:30:22 -0700

    complete

---
+++ bamazonSupervisor.js
@@ -0,0 +1,99 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    chooseOptions();
+    // buyProducts();
+});
+
+function chooseOptions() {
+    inquirer
+        .prompt(
+            [{
+                type: "list",
+                message: "menu options",
+                choices: [
+                    "View Product sales by department",
+                    "Create new department"
+                ],
+                name: "options"
+            }]
+        ).then(function (response) {
+
+            switch (response.options) {
+                case "View Product sales by department":
+                    displayProductSales();
+                    break;
+                case "Create new department":
+                    CreateNewDepartment();
+                    break;
+
+            }
+        });
+};
+
+function displayProductSales() {
+    connection.query("SELECT departments.department_id,departments.department_name,departments.over_head_costs, SUM(product_sales),SUM(product_sales)-departments.over_head_costs AS total_profit FROM departments INNER JOIN products ON departments.department_name=products.department_name GROUP BY departments.department_name ORDER BY department_id;",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            exit();
+        });
+};
+function CreateNewDepartment() {
+    inquirer
+        .prompt(
+            [{
+                type: "input",
+                message: "department_name",
+                name: "department_name"
+            }, {
+                type: "input",
+                message: "over_head_costs",
+                name: "over_head_costs"
+            }
+            ]
+        ).then(function (response) {
+            var departmentName = response.department_name;
+            var overHeadCosts = parseFloat(response.over_head_costs);
+
+            connection.query("INSERT INTO departments (department_name,over_head_costs) VALUES(?,?)", [departmentName, overHeadCosts],
+                function (err, res) {
+                    if (err) throw err;
+                    connection.query("SELECT * FROM departments",
+                        function (err, res) {
+                            if (err) throw err;
+                            console.table(res);
+                            exit();
+                        });
+                });
+        });
+};
+
+function exit() {
+    inquirer
+        .prompt({
+            type: "list",
+            message: "Do you want to continue or exit",
+            name: "isExit",
+            choices: ["continue", "exit"]
+        })
+        .then(function (response) {
+            switch (response.isExit) {
+                case "continue":
+                    chooseOptions();
+                    break;
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+};
--- departments.sql
+++ departments.sql
@@ -3,6 +3,17 @@ DROP TABLE IF EXISTS departments;
 CREATE TABLE departments(
     department_id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(50) NOT NULL,
-    over_head_costs DECIMAL(8,2) NOT NULL,
+    over_head_costs DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (department_id)
 )
+
+INSERT INTO departments(department_name,over_head_costs)
+VALUES("clothing",1000);
+INSERT INTO departments(department_name,over_head_costs)
+VALUES("electonics",1000);
+INSERT INTO departments(department_name,over_head_costs)
+VALUES("food",1000);
+INSERT INTO departments(department_name,over_head_costs)
+VALUES("sports",1000);
+INSERT INTO departments(department_name,over_head_costs)
+VALUES("books",1000);
